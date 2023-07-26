c3433e94498fe7588d34b6e1adc3415dd83182b6
Noukin <noukin68@gmail.com>
2023-07-26 21:36:19 +0300

    edit6

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 var db  = mysql.createPool({
   connectionLimit : 10,
